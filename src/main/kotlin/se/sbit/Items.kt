d65777d94b3cbfe750b6318a80ac97a23647ea22
Jöns Weimarck <jons.weimarck@gmail.com>
2023-01-08 16:18:26 +0100

    changed Carried from being a data class to an object, as it doesn't contain any data

--- src/main/kotlin/se/sbit/Items.kt
+++ src/main/kotlin/se/sbit/Items.kt
@@ -1,55 +1,51 @@
 package se.sbit
 
 
-typealias ItemsPlacementMap = Map<ItemType, Placement<ItemType, Room>>
+typealias ItemsPlacementMap = Map<ItemType, Placement<Room>>
 
 interface ItemType {
     val description: String
 }
 
-sealed class Placement<out Item, out Room>
-data class Carried<out Item>(val item: Item) : Placement<Item, Nothing>()
-data class InRoom<out Item, out Room>(val item: Item, val room: Room): Placement<Item, Room>()
+sealed class Placement<out Room>
+object Carried : Placement<Nothing>()
+data class InRoom<out Room>(val room: Room): Placement<Room>()
 
 class Items(initialItemMap: ItemsPlacementMap) {
 
-    private val itemMap: MutableMap<ItemType, Placement<ItemType, Room>> = initialItemMap.toMutableMap()
+    private val itemMap: MutableMap<ItemType, Placement<Room>> = initialItemMap.toMutableMap()
 
 
-    fun carriedItems(): List<ItemType> = itemMap.values
-        .filter{it is Carried<ItemType>}
-        .map{it as Carried<ItemType>}
-        .map{it.item}
+    fun carriedItems(): List<ItemType> = itemMap.keys.filter{itemMap[it] is Carried}
 
-    fun itemsIn(room: Room): List<ItemType> = itemMap.values
-        .filter{it is InRoom<ItemType, Room>}
-        .map{it as InRoom<ItemType, Room>}
-        .filter{it.room == room}
-        .map{it.item}
+    fun itemsIn(room: Room): List<ItemType> = itemMap.entries
+        .filter{it.value is InRoom<Room>}
+        .filter{(it.value as InRoom).room == room}
+        .map{it.key}
 
 
     fun pickUp(item: ItemType, currentRoom:Room): ItemType {
         // Sanity checks
         if(itemMap.containsKey(item)) {
-            if(itemMap[item] is Carried<ItemType>) {
+            if(itemMap[item] is Carried) {
                 throw Exception("Tried to carry already carried item")
             }
-            if((itemMap[item] as InRoom<ItemType, Room>).room != currentRoom){
+            if((itemMap[item] as InRoom<Room>).room != currentRoom){
                 throw Exception("Tried to pick up something from another room")
             }
         }
 
-        itemMap[item] = Carried(item)
+        itemMap[item] = Carried
         return item;
     }
 
     fun drop(item: ItemType, room: Room): ItemType {
         // Sanity checks
-        if(itemMap.containsKey(item) && (itemMap[item]!! is InRoom<ItemType, Room>)) {
+        if(itemMap.containsKey(item) && (itemMap[item]!! is InRoom<Room>)) {
             throw Exception("Tried to drop not carried item")
         }
 
-        itemMap[item] = InRoom(item, room)
+        itemMap[item] = InRoom(room)
         return item;
     }
 
