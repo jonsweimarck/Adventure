f5759f7d81b06a254e74816f0421925e1f10d4b9
Jöns Weimarck <jons.weimarck@gmail.com>
2023-02-08 21:41:59 +0100

    Refactoring pick up and drop to use EventLog (9/n). Out on a branch! Continuing to make Pickup/drop tests work. Mayby only vleaning up and refactor sample game left?

--- src/main/kotlin/se/sbit/adventure/engine/Items.kt
+++ src/main/kotlin/se/sbit/adventure/engine/Items.kt
@@ -41,15 +41,18 @@ data class InRoom(val room: Room): Placement()
 
 abstract class ItemPickedOrDropped(gameText: String, roomAndState: Pair<Room, State>, character: Character, val item: ItemType ): Event(gameText, roomAndState, character)
 abstract class ItemPickedOrDropped2(gameText: String, roomAndState: Pair<Room, State>, character: Character, val item: Item ): Event(gameText, roomAndState, character)
-class PickedUpItemEvent(gameText: String, roomAndState: Pair<Room, State>, character: Character,  item: ItemType):ItemPickedOrDropped(gameText, roomAndState, character, item)
-class PickedUpItemEvent2(gameText: String, roomAndState: Pair<Room, State>, character: Character,  item: Item):ItemPickedOrDropped2(gameText, roomAndState, character, item)
-class DroppedItemEvent(gameText: String,  roomAndState: Pair<Room, State>, character: Character,  item: ItemType):ItemPickedOrDropped(gameText,roomAndState, character, item)
-class DroppedItemEvent2(gameText: String,  roomAndState: Pair<Room, State>, character: Character,  item: Item):ItemPickedOrDropped2(gameText,roomAndState, character, item)
 
+
+@Deprecated("") class PickedUpItemEvent(gameText: String, roomAndState: Pair<Room, State>, character: Character,  item: ItemType):ItemPickedOrDropped(gameText, roomAndState, character, item)
+@Deprecated("") class DroppedItemEvent(gameText: String,  roomAndState: Pair<Room, State>, character: Character,  item: ItemType):ItemPickedOrDropped(gameText,roomAndState, character, item)
+
+class DroppedItemEvent2(gameText: String,  roomAndState: Pair<Room, State>, character: Character,  item: Item):ItemPickedOrDropped2(gameText,roomAndState, character, item)
+class PickedUpItemEvent2(gameText: String, roomAndState: Pair<Room, State>, character: Character,  item: Item):ItemPickedOrDropped2(gameText, roomAndState, character, item)
 class NoSuchItemHereEvent(gameText: String, roomAndState: Pair<Room, State>):Event(gameText, roomAndState)
 class NoSuchItemToDropItemEvent(gameText: String, roomAndState: Pair<Room, State>):Event(gameText, roomAndState)
 class InventoryEvent(gameText: String, roomAndState: Pair<Room, State>):Event(gameText, roomAndState)
 
+@Deprecated("")
 fun actionForPickUpItem(itemToPickUp:ItemType, noSuchItemHereEventText: String = "That didn't work!", pickedUpEventText: String = "Picked up"): (Input, EventLog, Items) -> Event
 {
     return fun(input, eventLog, items): Event {
@@ -62,10 +65,29 @@ fun actionForPickUpItem(itemToPickUp:ItemType, noSuchItemHereEventText: String =
     }
 }
 
+fun actionForPickUpItem2(itemToPickUp:Item, noSuchItemHereEventText: String = "That didn't work!", pickedUpEventText: String = "Picked up"): (Input, EventLog, Items2) -> Event
+{
+    return fun(input, eventLog, items): Event {
+        val currentRoomAndState = eventLog.getCurrentRoomAndState(Player)
+        val currentRoom = currentRoomAndState.first
+        if (itemsIn2(currentRoom, eventLog).none { it == itemToPickUp }){
+            return NoSuchItemHereEvent(noSuchItemHereEventText, currentRoomAndState)
+        }
+        return PickedUpItemEvent2("$pickedUpEventText ${itemToPickUp.description(eventLog)}.", currentRoomAndState, Player, itemToPickUp)
+    }
+}
 
+@Deprecated("")
 fun actionForExamineItem(itemToExam: ItemType, successGameText: String= "You don't see anything special", failureGameText:String = "You don't carry that" ): (Input, EventLog, Items) -> Event =
     fun(_, eventLog, items): Event =
         when(carriedItems(eventLog).contains(itemToExam)) {
+            true -> Event(successGameText, eventLog.getCurrentRoomAndState(Player))
+            false -> Event(failureGameText, eventLog.getCurrentRoomAndState(Player))
+        }
+
+fun actionForExamineItem2(itemToExam: Item, successGameText: String= "You don't see anything special", failureGameText:String = "You don't carry that" ): (Input, EventLog, Items2) -> Event =
+    fun(_, eventLog, items): Event =
+        when(carriedItems2(eventLog).contains(itemToExam)) {
             true -> Event(successGameText, eventLog.getCurrentRoomAndState(Player))
             false -> Event(failureGameText, eventLog.getCurrentRoomAndState(Player))
         }
@@ -103,6 +125,22 @@ fun goActionForInventory(notCarryingAnythingEventText: String = "You don't carry
     }
 }
 
+
+fun itemsIn2(room: Room, eventLog: EventLog): List<Item> {
+    var itemSet = emptySet<Item>().toMutableSet()
+    eventLog.log().forEach{
+        if(it.roomAndState.first == room) {
+            if (it is DroppedItemEvent2) {
+                itemSet.add(it.item)
+            } else if (it is PickedUpItemEvent2) {
+                itemSet.remove(it.item)
+            }
+        }
+    }
+    return itemSet.toList()
+}
+
+@Deprecated("")
 fun itemsIn(room: Room, eventLog: EventLog): List<ItemType> {
     var itemSet = emptySet<ItemType>().toMutableSet()
     eventLog.log().forEach{
@@ -115,22 +153,6 @@ fun itemsIn(room: Room, eventLog: EventLog): List<ItemType> {
         }
     }
     return itemSet.toList()
-
-
-//    var itemMap = emptyMap<ItemType, Int>().toMutableMap()
-//    eventLog.log().forEach{
-//        if(it.roomAndState.first == room){
-//            if(it is DroppedItemEvent){
-//                var pickedAndDrop: Int = itemMap[it.item] ?: 0
-//                itemMap[it.item] = pickedAndDrop +1
-//            } else if(it is PickedUpItemEvent){
-//                var pickedAndDrop: Int = itemMap[it.item] ?: 0
-//                itemMap[it.item] = pickedAndDrop -1
-//            }
-//        }
-//    }
-//
-//    return itemMap.filter { it.value == 1 }.keys.toList()
 }
 
 
